c73fc708858fc68ff203b298756b58fc909d41f2
0x3ea <xdfrse@gmail.com>
2021-11-18 01:20:12 +0200

    Create class NetConfAgent

---
+++ src/netconf_agent/include/netconf_agent.hpp
@@ -0,0 +1,8 @@
+class NetConfAgent {
+ public:
+  void Test();
+ private:
+  bool session;
+  bool connection;
+  bool subscribe;
+};
---
+++ src/netconf_agent/source/netconf_agent.cpp
@@ -0,0 +1,7 @@
+#include "netconf_agent.hpp"
+
+#include <iostream>
+
+void NetConfAgent::Test() {
+  std::cout << "NetConfAgent.Test()\n";
+}
