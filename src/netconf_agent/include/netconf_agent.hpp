3481551ebec072d6cb51a2f98095b7b769b85648
0x3ea <xdfrse@gmail.com>
2021-11-23 22:30:09 +0200

    Implement method SubscribeForModelChanges of class NetConfAgent

--- src/netconf_agent/include/netconf_agent.hpp
+++ src/netconf_agent/include/netconf_agent.hpp
@@ -1,8 +1,22 @@
+#ifndef MOBILE_NETWORK_NETCONF_AGENT_INCLUDE_HPP_
+#define MOBILE_NETWORK_NETCONF_AGENT_INCLUDE_HPP_
+
+#include <sysrepo-cpp/Connection.hpp>
+#include <sysrepo-cpp/Session.hpp>
+#include <sysrepo-cpp/Subscription.hpp>
+
+#include <optional>
+
 class NetConfAgent {
  public:
-  void Test();
+  NetConfAgent(const char* module_name);
+
+  bool SubscribeForModelChanges(const char* module_name);
+
  private:
-  bool session;
-  bool connection;
-  bool subscribe;
+  sysrepo::Connection conn;
+  sysrepo::Session sess;
+  std::optional<sysrepo::Subscription> sub;
 };
+
+#endif  // MOBILE_NETWORK_NETCONF_AGENT_INCLUDE_HPP_
--- src/netconf_agent/source/netconf_agent.cpp
+++ src/netconf_agent/source/netconf_agent.cpp
@@ -1,7 +1,23 @@
 #include "netconf_agent.hpp"
 
-#include <iostream>
+#include <sysrepo-cpp/Session.hpp>
 
-void NetConfAgent::Test() {
-  std::cout << "NetConfAgent.Test()\n";
+#include <iostream>
+#include <string>
+
+NetConfAgent::NetConfAgent(const char* module_name)
+    : conn(),
+      sess(conn.sessionStart()) {
+  sess.copyConfig(sysrepo::Datastore::Startup, module_name);
+}
+
+bool NetConfAgent::SubscribeForModelChanges(const char* module_name) {
+  sysrepo::ModuleChangeCb module_change_cb = [] (sysrepo::Session session, auto, auto, auto, auto, auto) -> sysrepo::ErrorCode {
+    for (const auto& change : session.getChanges()) {
+      std::cout << "\nChanges: " << std::string{change.node.path()} << '\n';
+    }
+    return sysrepo::ErrorCode::Ok;
+  };
+  sub = sess.onModuleChange(module_name, module_change_cb, nullptr, 0, sysrepo::SubscribeOptions::DoneOnly);
+  return true;
 }
